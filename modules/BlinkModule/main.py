64c0683a5f5d67e08869f59630c2fb0412cd5c4b
Ankit Khokhar <ankhokha@microsoft.com>
2019-05-15 04:57:25 +0000

    Updated main.py

--- modules/BlinkModule/main.py
+++ modules/BlinkModule/main.py
@@ -16,8 +16,8 @@ def main(protocol):
     GPIO.setup(18, GPIO.OUT)
     while True:
         GPIO.output(18, GPIO.HIGH)
-        sleep(1)
-        GPIO.output(18, GPIO.LOW)
+        #sleep(1)
+        #GPIO.output(18, GPIO.LOW)
         sleep(1)
     """from time import sleep
     led = LED(17)
